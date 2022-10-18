fe1e78bbfe2602a19de266e6d585b4cafb49ce41
qodirovoybekjon4929 <qodirovoybekjon08@gmail.com>
2022-10-18 14:06:22 +0500

    w-code

---
+++ src/components/users.tsx
@@ -0,0 +1,14 @@
+interface UTypes {
+    id: number
+    fullname: string
+}
+
+function users() {
+    return (
+        <div>
+            users-component
+        </div>
+    )
+}
+
+export default users
--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,8 +1,15 @@
+import { useEffect, useState } from "react"
+import Users from "../components/users"
+
+interface UTypes {
+    id: number
+    fullname: string
+}
+
 function UsersPage() {
+    const [usersData, setUsers] = useState([])
     return (
-        <div>
-            users-page
-        </div>
+        <Users />
     )
 }
 
